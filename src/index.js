7acd682af075dcfb81f25dc64cb0952ae7dfb66a
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-04-12 03:31:10 +0500

    create webpack template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('hy')
+import './style.css';
+import Score from './modules/score.js';
+
+const newScore = new Score();
+const addScore = document.querySelector('.input-user');
+
+addScore.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const score = document.getElementById('scores').value;
+  newScore.addList({ name, score });
+
+  addScore.name.value = '';
+  addScore.scores.value = '';
+});
+
+newScore.init();
